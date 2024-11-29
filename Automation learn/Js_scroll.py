c0b9097a84f04871d43b45f2117f13d1e641dc60
bons <bons6710hos@gmail.com>
2024-11-29 17:01:38 +0300

    Initial commit

--- Automation learn/Js_scroll.py
+++ Automation learn/Js_scroll.py
@@ -15,7 +15,7 @@ driver.implicitly_wait(5)
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 print(driver.title)
-driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+# driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
 
 driver.execute_script("window.scrollBy(0,700)")
 
